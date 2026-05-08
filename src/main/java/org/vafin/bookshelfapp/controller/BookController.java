c65327623564e220e63ea7b75fdc659e733b0963
Oleksandr Vafin <vafinalex@gmail.com>
2026-05-08 18:24:30 +0200

    Implement RestController: BookController

---
+++ src/main/java/org/vafin/bookshelfapp/controller/BookController.java
@@ -0,0 +1,48 @@
+package org.vafin.bookshelfapp.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.vafin.bookshelfapp.model.Book;
+import org.vafin.bookshelfapp.service.BookService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+@RequiredArgsConstructor
+public class BookController {
+    private final BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping()
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookService.createBook(book));
+    }
+
+    //Update book
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
+        return ResponseEntity.ok(bookService.updateBook(id, book));
+    }
+
+    //Delete book
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+        return ResponseEntity.noContent().build();
+    }
+}
