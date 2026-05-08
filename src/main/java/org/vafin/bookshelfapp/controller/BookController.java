d389faaf244ac6c7e0cc023d8cae939da0ebaa75
Oleksandr Vafin <vafinalex@gmail.com>
2026-05-08 19:28:51 +0200

    Remove redundant import

--- src/main/java/org/vafin/bookshelfapp/controller/BookController.java
+++ src/main/java/org/vafin/bookshelfapp/controller/BookController.java
@@ -2,7 +2,6 @@ package org.vafin.bookshelfapp.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.vafin.bookshelfapp.model.Book;
