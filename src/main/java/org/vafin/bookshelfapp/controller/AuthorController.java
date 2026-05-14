7c7a97965cce1c09f3a11d1485476e29abc6c82a
Oleksandr Vafin <vafinalex@gmail.com>
2026-05-14 21:02:59 +0200

    Finish Author controller

--- src/main/java/org/vafin/bookshelfapp/controller/AuthorController.java
+++ src/main/java/org/vafin/bookshelfapp/controller/AuthorController.java
@@ -1,12 +1,15 @@
 package org.vafin.bookshelfapp.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.vafin.bookshelfapp.dto.AuthorDTO;
 import org.vafin.bookshelfapp.model.Author;
 import org.vafin.bookshelfapp.service.AuthorService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/authors")
@@ -15,22 +18,32 @@ public class AuthorController {
     private final AuthorService authorService;
 
     @GetMapping
-    public ResponseEntity<List<Author>> getAllAuthors() {
-        return ResponseEntity.ok(authorService.getAllAuthors());
+    public ResponseEntity<List<AuthorDTO>> getAllAuthors() {
+        List<AuthorDTO> authors = authorService.getAllAuthors()
+                .stream()
+                .map(AuthorDTO::from)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(authors);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AuthorDTO> getAuthorById(@PathVariable Long id) {
+        AuthorDTO author = AuthorDTO.from(authorService.getAuthorById(id));
+        return ResponseEntity.ok(author);
     }
 
     @PostMapping
-    public ResponseEntity<Author> createAuthor(@RequestBody Author author) {
-        return ResponseEntity.ok(authorService.addAuthor(author));
+    public ResponseEntity<AuthorDTO> createAuthor(@RequestBody Author author) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(AuthorDTO.from(authorService.addAuthor(author)));
     }
 
-    @PutMapping
-    public ResponseEntity<Author> updateAuthor(@RequestParam Long id, @RequestBody Author author) {
-        return ResponseEntity.ok(authorService.updateAuthor(id, author));
+    @PutMapping("/{id}")
+    public ResponseEntity<AuthorDTO> updateAuthor(@PathVariable Long id, @RequestBody Author author) {
+        return ResponseEntity.ok(AuthorDTO.from(authorService.updateAuthor(id, author)));
     }
 
-    @DeleteMapping
-    public ResponseEntity<Void> deleteAuthor(@RequestParam Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAuthor(@PathVariable Long id) {
         authorService.deleteAuthor(id);
         return ResponseEntity.noContent().build();
     }
