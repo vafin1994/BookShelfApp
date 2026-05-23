0d48be12e23fd1fe9031edc50a95f83257afc2f8
Oleksandr Vafin <vafinalex@gmail.com>
2026-05-23 19:36:04 +0200

    Allow requests from Vercel

--- src/main/java/org/vafin/bookshelfapp/config/CorsConfig.java
+++ src/main/java/org/vafin/bookshelfapp/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://*.vercel.app")
+                .allowedOrigins("http://localhost:4200", "https://bookshelfapp-indol.vercel.app")
                 .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name())
                 .allowedHeaders("*");
     }
