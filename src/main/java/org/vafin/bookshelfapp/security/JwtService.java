3bc2147b69f88d053ed576c702229fbdef3a4766
Oleksandr Vafin <vafinalex@gmail.com>
2026-05-27 19:44:24 +0200

    Add JWT service

--- src/main/java/org/vafin/bookshelfapp/security/JwtService.java
+++ src/main/java/org/vafin/bookshelfapp/security/JwtService.java
@@ -1,5 +1,59 @@
 package org.vafin.bookshelfapp.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Objects;
+
+@Service
 public class JwtService {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .claim("role", userDetails.getAuthorities())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        Claims claims = extractAllClaims(token);
+        return claims.getSubject();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        Claims claims = extractAllClaims(token);
+        String email = claims.getSubject();
+        Date expiry = claims.getExpiration();
+        return (Objects.equals(email, userDetails.getUsername()) && expiry.after(new Date()));
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
 
 }
